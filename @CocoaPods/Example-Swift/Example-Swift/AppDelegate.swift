403e20706abc0b56228568eb39127d726d34bb81
su0928597196 <53365914+su0928597196@users.noreply.github.com>
2026-09-01 15:16:40 +0800

    Add files via upload

---
+++ @CocoaPods/Example-Swift/Example-Swift/AppDelegate.swift
@@ -0,0 +1,47 @@
+//
+//  AppDelegate.swift
+//  Example-Swift
+//
+//  Created by ECPay.
+//  Copyright © 2020 ECPay. All rights reserved.
+//
+
+import UIKit
+import ECPayPaymentGatewayKit
+import IQKeyboardManagerSwift
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+//        ECPayPaymentGatewayManager().testIQKeyboard()
+//        ECPayPaymentGatewayManager().testAPI()
+
+        IQKeyboardManager.shared.enable = true
+        
+        var envStr:String = "prod"
+        if let env = Bundle.main.object(forInfoDictionaryKey: "ENV") as? String {
+            envStr = env.lowercased()
+        }
+        
+        switch envStr {
+        case "beta":
+            ECPayPaymentGatewayManager.sharedInstance().initialize(env: .Beta)
+            break
+        case "stage":
+            ECPayPaymentGatewayManager.sharedInstance().initialize(env: .Stage)
+            break
+        default:
+            ECPayPaymentGatewayManager.sharedInstance().initialize(env: .Prod)
+            break
+        }
+        
+        
+        return true
+    }
+
+}
+
