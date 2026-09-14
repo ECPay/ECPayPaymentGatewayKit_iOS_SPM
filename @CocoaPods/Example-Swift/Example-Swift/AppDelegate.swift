419538b87fad995f3c844d190bed8eae6e7fa56a
Aaron Yen <yen064@gmail.com>
2026-09-14 16:45:00 +0800

    version 1.10.0

--- @CocoaPods/Example-Swift/Example-Swift/AppDelegate.swift
+++ @CocoaPods/Example-Swift/Example-Swift/AppDelegate.swift
@@ -16,30 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-//        ECPayPaymentGatewayManager().testIQKeyboard()
-//        ECPayPaymentGatewayManager().testAPI()
 
+        // 範例
         IQKeyboardManager.shared.enable = true
-        
-        var envStr:String = "prod"
-        if let env = Bundle.main.object(forInfoDictionaryKey: "ENV") as? String {
-            envStr = env.lowercased()
-        }
-        
-        switch envStr {
-        case "beta":
-            ECPayPaymentGatewayManager.sharedInstance().initialize(env: .Beta)
-            break
-        case "stage":
-            ECPayPaymentGatewayManager.sharedInstance().initialize(env: .Stage)
-            break
-        default:
-            ECPayPaymentGatewayManager.sharedInstance().initialize(env: .Prod)
-            break
-        }
-        
-        
+        ECPayPaymentGatewayManager.sharedInstance().initialize(env: .Stage)
         return true
     }
 
